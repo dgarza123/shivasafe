d5fde715cb3d5e9ad7f22289a5a1f906d966145f
David Garzotto <135085722+dgarza123@users.noreply.github.com>
2025-04-14 20:57:44 -0700

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -1,7 +1,8 @@
 import streamlit as st
+st.set_page_config(page_title="Login", layout="centered")
+
 from login_manager import login, logout, is_logged_in, current_user, current_role
 
-st.set_page_config(page_title="Login", layout="centered")
 st.title("🔐 Login")
 
 if is_logged_in():
